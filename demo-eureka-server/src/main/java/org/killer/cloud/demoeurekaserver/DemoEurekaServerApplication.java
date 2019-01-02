a8b145433b2b4cd8f5b2dd567e542d46622287c3
killerWqs <1249476676@qq.com>
2019-01-03 00:07:30 +0800

    update content

--- demo-eureka-server/src/main/java/org/killer/cloud/demoeurekaserver/DemoEurekaServerApplication.java
+++ demo-eureka-server/src/main/java/org/killer/cloud/demoeurekaserver/DemoEurekaServerApplication.java
@@ -2,8 +2,8 @@ package org.killer.cloud.demoeurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
 @EnableEurekaServer
 @SpringBootApplication
 public class DemoEurekaServerApplication {
