3d3086769e78a80a922f6bc615cbef050fca3da9
killerWqs <1249476676@qq.com>
2019-01-09 02:40:33 +0800

    update content

---
+++ demo-config-server/src/main/java/org/killer/cloud/democonfigserver/DemoConfigServerApplication.java
@@ -0,0 +1,16 @@
+package org.killer.cloud.democonfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class DemoConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoConfigServerApplication.class, args);
+    }
+
+}
+
