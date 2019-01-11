de0b51f863539006a3d803d8409318753699a7c7
killerWqs <1249476676@qq.com>
2019-01-11 15:30:26 +0800

    update content

--- demo-config-server/src/main/java/org/killer/cloud/democonfigserver/DemoConfigServerApplication.java
+++ demo-config-server/src/main/java/org/killer/cloud/democonfigserver/DemoConfigServerApplication.java
@@ -3,8 +3,10 @@ package org.killer.cloud.democonfigserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableConfigServer
+@EnableEurekaClient
 @SpringBootApplication
 public class DemoConfigServerApplication {
 
